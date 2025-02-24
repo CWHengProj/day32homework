dc9eed9d5ac98a4c2181ec53425e9e4e965a5963
CW <142023227+CWHengProj@users.noreply.github.com>
2025-02-24 15:13:21 +0800

    completed homework32

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,6 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input, OnInit } from '@angular/core';
 import { Fruit } from '../../../model/Fruits';
-import { FormBuilder } from '@angular/forms';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -8,9 +8,43 @@ import { FormBuilder } from '@angular/forms';
   templateUrl: './form.component.html',
   styleUrl: './form.component.css'
 })
-export class FormComponent {
+export class FormComponent implements OnInit{
+  
   @Input()
   cart:Fruit[]=[]
-  fb:FormBuilder=new FormBuilder()
-  
+  private fb = inject(FormBuilder)
+  //form is a formGroup
+  userForm!:FormGroup
+  //the table is a formArray
+  userArray!:FormArray
+  total:number=0
+  ngOnInit(): void {
+    this.createForm()
+  }
+  protected createForm(){
+    //when init, instantiate the types so they can be displayed
+    this.userArray = this.fb.array([])
+    this.userForm= this.fb.group(
+      {
+        name:this.fb.control<string>('',[Validators.required, Validators.minLength(5)]),
+        address:this.fb.control<string>('',[Validators.required, Validators.minLength(5)]),
+        deliveryType:this.fb.control<string>('delivery',[Validators.required]),
+        userArray: this.userArray
+      }
+    )
+  }
+  protected submitForm(){
+    console.log('Submitted! ',this.userForm)
+  }
+  protected invalid(){
+    return this.userForm.invalid
+  }
+  protected totalCost():number{
+    this.total=0
+    this.cart.forEach((item:Fruit)=>{
+      this.total+=(item.quantity * item.price)
+    })
+    return this.total;
+  }
+        
 }
